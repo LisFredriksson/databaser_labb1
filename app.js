1e9557127a8288be25b55996626cf27d8db9ad24
Lis Fredriksson <lisfredrikson@gmail.com>
2023-04-25 10:36:09 +0200

    more settings

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
-
 //middlewere & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.redirect('/home')
+});
+app.get('/home', (req, res) => {
+    //res.send(<h1>ABOUT</h1>);
+    res.render('home', { title: 'Home' })
+});
+
+
+//blog routes
+const libraryRoutes = require('./routes/libraryRoutes')
+app.use('/library', libraryRoutes)
+
+//404 page (must be at the bottom)
+app.use(() => {
+    res.status(404).render('404', { title: '404' })
+})
+
+app.listen(3000);
---
+++ library_db.js
@@ -0,0 +1,32 @@
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:Nallepuh9988@localhost:5432/dvdrental')
+
+async function selectAllCustomers() {
+
+    let data = await db.many("SELECT * FROM customer")
+
+    return data;
+}
+
+async function selectAllCities() {
+
+    let data = await db.many("SELECT * FROM city")
+
+    return data;
+}
+
+async function insertCity() {
+
+    await db.none(`INSERT INTO city(city, country_id) VALUES(${name}, ${country_id})`)
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+
+    return result;
+}
+
+module.exports = {
+    selectAllCustomers,
+    selectAllCities,
+    insertCity
+}
