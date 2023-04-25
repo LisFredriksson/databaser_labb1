ea8cbd13a25e2d5cc90720ead625c9608ef39176
Lis Fredriksson <lisfredrikson@gmail.com>
2023-04-25 18:30:30 +0200

    fix

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/bookControllers');
 
-router.get("/all", async (req, res) => {
+router.get("/", async (req, res) => {
     controller.get(req, res)
 });
 
 router.post("/create", async (req, res) => {
     controller.add(req, res);
     console.log(req.body)
-
-
 });
 
 module.exports = router;
